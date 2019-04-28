8dfd33337cd23949454349da52b95da3767440b7
yms8421 <46646219+yms8421@users.noreply.github.com>
2019-04-28 14:11:58 +0300

    Add files via upload

--- DML.sql
+++ DML.sql
@@ -135,3 +135,69 @@ FROM Orders o
 WHERE o.EmployeeID = (SELECT EmployeeId  
 					  FROM Employees
 					  WHERE FirstName = 'Nancy' AND LastName = 'Davolio')
+USE Northwind
+SELECT TOP 5 * FROM Customers
+--Londra'daki müþterilerin aldýðý ürünler ve tarihleri
+--Can PERK         |  Chai  |  2019-04-28
+--Gamze EFENDÝOÐLU |  Ikura |  2019-04-12
+SELECT c.ContactName, p.ProductName, CONVERT (varchar, o.OrderDate, 103) AS OrderDate FROM Customers c
+INNER JOIN Orders o ON c.CustomerID = o.CustomerID 
+INNER JOIN [Order Details] d ON o.OrderID = d.OrderID
+INNER JOIN Products p ON p.ProductID = d.ProductID
+WHERE c.City = 'london'
+ORDER BY c.ContactName, p.ProductName
+
+--INNER JOIN : Kesin eþlemþme
+--LEFT JOIN  : sol tablo kesin gelecek, sað tablo eþleþenleri getirecek
+--RIGHT JOIN : sað tablo kesin gelecek, sol tablo eþleþenleri getirecek
+--CROSS JOIN : komple eþleþme (kartezyen)
+--OUTER JOIN*: kullanýldýðý join türüne göre davranýr
+
+SELECT * FROM Categories WHERE CategoryName = 'Electronics'
+SELECT * FROM Products p 
+CROSS JOIN Categories c --ON p.CategoryID = c.CategoryID
+WHERE p.CategoryID = 9
+
+USE AdventureWorks
+SELECT * FROM HumanResources.Employee WHERE BusinessEntityID = 250
+SELECT * FROM Person.Person WHERE BusinessEntityID = 250
+SELECT * FROM Person.EmailAddress WHERE BusinessEntityID = 250
+
+SELECT e.LoginID, h.StartDate, d.Name, d.GroupName 
+FROM HumanResources.Employee e
+INNER JOIN HumanResources.EmployeeDepartmentHistory h ON e.BusinessEntityID = h.BusinessEntityID
+INNER JOIN HumanResources.Department d ON d.DepartmentID = h.DepartmentID
+WHERE e.BusinessEntityID = 250;
+
+--Ürün Kategorileri
+SELECT c.Name CategoryName,  p.Name AS ProductName FROM Production.Product p
+INNER JOIN Production.ProductSubcategory s ON s.ProductSubcategoryID = p.ProductSubcategoryID
+INNER JOIN Production.ProductCategory c ON s.ProductCategoryID = c.ProductCategoryID
+WHERE c.Name = 'Bikes'
+ORDER BY CategoryName, ProductName
+--Ada göre ürün arama
+SELECT * FROM Production.Product 
+WHERE Name LIKE '% red %'
+--Hangi ürün hangi depoda kaç tane var
+SELECT p.Name [Product Name], l.Name AS [Store Name], i.Quantity
+FROM Production.ProductInventory i
+INNER JOIN Production.Location l ON i.LocationID = l.LocationID
+INNER JOIN Production.Product p ON p.ProductID = i.ProductID
+ORDER BY p.Name, i.Quantity DESC
+
+--Hangi üründen kaç tane var
+--AGGREGATE FUNCTION
+SELECT p.Name [Product Name], SUM(i.Quantity) AS Summary
+FROM Production.ProductInventory i
+INNER JOIN Production.Location l ON i.LocationID = l.LocationID
+INNER JOIN Production.Product p ON p.ProductID = i.ProductID
+GROUP BY p.Name --haftaya bakacaðýz
+ORDER BY p.Name
+
+--Hangi depoda kaç ürün var
+SELECT l.Name, SUM(i.Quantity) AS Summary
+FROM Production.ProductInventory i
+INNER JOIN Production.Location l ON i.LocationID = l.LocationID
+INNER JOIN Production.Product p ON p.ProductID = i.ProductID
+GROUP BY l.Name
+ORDER BY l.Name
